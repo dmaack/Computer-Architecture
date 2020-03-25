391927822ff962ac628025843c13848aded60ac5
Dominique Maack <dominiquemaack@Dominiques-MBP.domain>
2020-03-24 18:34:46 -0600

    step1 completed, un-hardcoded machine code

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -17,23 +17,50 @@ class CPU:
     def load(self):
         """Load a program into memory."""
 
-        address = 0
+        # address = 0
 
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001, # HLT
+        # ]
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        # for instruction in program:
+        #     self.ram[address] = instruction
+        #     address += 1
+
+        try:
+            address = 0 
+            cmd_file = sys.argv[1]
+
+            # open file: save appropriate data to RAM
+            with open(cmd_file) as f:
+                # read the content line by line
+                for line in f:
+                    # remove '#' comments
+                    cmd_split = line.split('#')
+                    # remove spaces
+                    cmd = cmd_split[0].strip()
+
+                    if cmd == '':
+                        # ignore black lines
+                        continue
+
+                    value = int(cmd, 2)
+                    self.ram[address] = value
+                    address += 1
+
+        except FileNotFoundError:
+            print(f'Error: {cmd_file} file not found')
+            sys.exit()
+
+
 
 
     def alu(self, op, reg_a, reg_b):
